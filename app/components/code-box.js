192bc4200250939de41b14e274c4efaf0ca07f4a
Allison Larson <allisonlarson91@gmail.com>
2014-11-18 14:31:48 -0700

    set up component psuedo code

---
+++ app/components/code-box.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+// define actions that 
+//  get the code from the div
+//  send request to Rails/Eval.in via AJAX
+// 
+//  get response from ^^
+//  compare result with problem.solution
+//  decide success or failure
+//
+//  if success
+//    notify success
+//    activate submit button
+//
+//  else
+//    notify failure
+//
+// submit button action
+//  create solution record & save to Rails
+
+})
