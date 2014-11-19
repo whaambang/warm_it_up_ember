aaffdc59556d7c50baea83f8111b726b8eb5c901
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-18 21:23:10 -0700

    created form and the actions for each button

--- app/components/code-box.js
+++ app/components/code-box.js
@@ -1,23 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    submit: function() {
+      this.sendAction('submit', {
+        // submit button action
+        //  create solution record & save to Rails
+      });
+    },
 
-// define actions that 
-//  get the code from the div
-//  send request to Rails/Eval.in via AJAX
-// 
-//  get response from ^^
-//  compare result with problem.solution
-//  decide success or failure
-//
-//  if success
-//    notify success
-//    activate submit button
-//
-//  else
-//    notify failure
-//
-// submit button action
-//  create solution record & save to Rails
+    run: function() {
+      this.sendAction('run', {
+        //  get the code from the div
+        //  send request to Rails/Eval.in via AJAX
+        // define actions that
+        //  get the code from the div
+        //  send request to Rails/Eval.in via AJAX
+        //
+        //  get response from ^^
+        //  compare result with problem.solution
+        //  decide success or failure
+        //
+        //  if success
+        //    notify success
+        //    activate submit button
+        //
+        //  else
+        //    notify failure
+      });
+    }
 
 })
