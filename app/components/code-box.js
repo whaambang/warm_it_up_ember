c2873a39edbf6f7c9a3df60f5d7d5b377c9993bf
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-19 11:29:15 -0700

    getting post request working

--- app/components/code-box.js
+++ app/components/code-box.js
@@ -1,32 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  solution: this.get('problem.solution'),
+  solution: 'helloworld',
   actions: {
     submit: function() {
-      this.sendAction('submit', {
+      this.sendAction('submit', function(){
         //  create solution record & save to Rails
       });
     },
 
     run: function(solution) {
-      this.sendAction('run', function(solution){
-
-        //  get the code from the div
-            var answer;
-        //  send request to Rails/Eval.in via AJAX
-            $.post('/coderunner', {code: 'puts "Hello Wold"'})
-              .done(function(data, solution){
-                answer = data;
-                if(answer === solution){
-                  //success notification
-                  //activate submit button
-                }
-                else{
-                  //failure notification
-                }
-              });
-      });
+      //  get the code from the div
+      var answer;
+      //  send request to Rails/Eval.in via AJAX
+      $.post('http://0.0.0.0:3000/api/v1/coderunner', {code: 'puts "Hello Wold"'})
+        .done(function(data, solution){
+          answer = data;
+          debugger;
+          if(answer === solution){
+            //success notification
+            //activate submit button
+          }
+          else{
+            //failure notification
+          }
+        })
+        .fail(function(){
+          alert("error");
+        });
     }
   }
 });
