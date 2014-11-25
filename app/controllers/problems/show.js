ef4555decf3db9f72a37463843898eeaac4f4363
Marc Garreau <marcdgarreau@gmail.com>
2014-11-24 20:06:19 -0700

    Properly scope vars

--- app/controllers/problems/show.js
+++ app/controllers/problems/show.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     createSolution: function(answer) {
-      var timeAtSubmission = new Date();
-      var timeAtStart      = new Date().setHours(8,30,0,0);
-      var secondsFromStart = (timeAtSubmission - timeAtStart)/1000;
       var points = function () {
+        var timeAtSubmission = new Date();
+        var timeAtStart      = new Date().setHours(8,30,0,0);
+        var secondsFromStart = (timeAtSubmission - timeAtStart)/1000;
         if (parseInt(2000 - secondsFromStart, 10) > 0) {
           return parseInt(2000 - secondsFromStart, 10);
         } else {
