5c59f15cb133bbb9f85aba379b1c7e96a51efb85
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-25 15:54:00 -0700

    countdown is now clock

--- app/views/scores.js
+++ app/views/scores.js
@@ -3,15 +3,8 @@ import Ember from 'ember';
 export default Ember.View.extend({
   didInsertElement: function(){
     var date = new Date();
-    // date.setHours(8);
-    // date.setMinutes(30);
-    var clock = $('.countdown').FlipClock(1800, {
-      autoStart: false,
-      countdown: true,
-      clockFace: "MinuteCounter"
+    var clock = $('.clock').FlipClock({
+      clockFace: 'TwelveHourClock'
     });
-    if(date.getHours() === 8 && date.getMinutes() === 30){
-      clock.start();
-    }
   }
 });
