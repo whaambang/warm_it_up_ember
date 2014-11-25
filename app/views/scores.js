d9edad81868d8f569774335279ddd18cb09536a9
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-24 22:26:20 -0700

    clock starts at 8:30

--- app/views/scores.js
+++ app/views/scores.js
@@ -2,11 +2,16 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   didInsertElement: function(){
-    $('.countdown').FlipClock(1800, {
-      autoStart: true,
+    var date = new Date();
+    // date.setHours(8);
+    // date.setMinutes(30);
+    var clock = $('.countdown').FlipClock(1800, {
+      autoStart: false,
       countdown: true,
       clockFace: "MinuteCounter"
-
     });
+    if(date.getHours() === 8 && date.getMinutes() === 30){
+      clock.start();
+    }
   }
 });
