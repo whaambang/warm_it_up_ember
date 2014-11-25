d717550619328dbda984cf2580f4f67907e112df
Marc Garreau <marcdgarreau@gmail.com>
2014-11-24 18:57:38 -0700

    Refactors points method

--- app/controllers/problems/show.js
+++ app/controllers/problems/show.js
@@ -3,12 +3,12 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     createSolution: function(answer) {
-      var timeAtSubmission = new Date().getTime();
+      var timeAtSubmission = new Date();
       var timeAtStart      = new Date().setHours(8,30,0,0);
-      var secondsFromStart = timeAtSubmission - timeAtStart;
+      var secondsFromStart = (timeAtSubmission - timeAtStart)/1000;
       var points = function () {
-        if (parseInt((2000 - secondsFromStart), 10) > 0) {
-          return parseInt((2000 - (timeAtSubmission - timeAtStart))/1000, 10);
+        if (parseInt(2000 - secondsFromStart, 10) > 0) {
+          return parseInt(2000 - secondsFromStart, 10);
         } else {
           return 0;
         }
