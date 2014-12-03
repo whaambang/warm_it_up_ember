9e2c9d902d85d264b1471045267c200a8c80eb86
Marc Garreau <marcdgarreau@gmail.com>
2014-12-02 17:10:55 -0700

    Woops

--- app/models/solution.js
+++ app/models/solution.js
@@ -7,14 +7,10 @@ export default DS.Model.extend({
   points_earned: DS.attr('string'),
   votes: DS.hasMany('vote'),
   posse: DS.attr('string'),
-<<<<<<< HEAD
   problem: DS.belongsTo('problem'),
+  posse_image: DS.attr('string'),
 
   upvoteCount: function() {
     return this.get('votes.length')
   }.property('votes.length')
-=======
-  posse_image: DS.attr('string'),
-  problem: DS.belongsTo('problem')
->>>>>>> 6409c26e92d34974103dfee9049d393a3cd71166
 });
