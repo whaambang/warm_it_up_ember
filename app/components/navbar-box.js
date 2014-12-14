ea7208f486caf66dd28637f0af892a6563ee5cde
Will Faurot <wfaurot@gmail.com>
2014-12-14 14:55:19 -0700

    Reset date for problems

--- app/components/navbar-box.js
+++ app/components/navbar-box.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   problemNumber: function() {
-    var initialDate = new Date(2014, 10, 1); // Attention: month is zero-based
+    var initialDate = new Date(2014, 11, 13); // Attention: month is zero-based
     var now = Date.now();
     var difference = now - initialDate;
     var millisecondsPerDay = 24 * 60 * 60 * 1000;
     var daysSince = Math.floor(difference / millisecondsPerDay);
     return daysSince;
   }.property()
-})
+});
